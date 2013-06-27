cc8e7e20aafea89799fc3669c0fba314c435cf92
Morten Albring <mortenalbring@gmail.com>
2013-06-27 17:07:25 +0100

    Zoom plot

--- js/core.js
+++ js/core.js
@@ -10,139 +10,6 @@ function readText(filePath) {
         return output;
     }   
 
-//place contents of text file into 2d array
-parseSpectra = function (txt) {
-	var txt_lines = txt.split('\n');
-	var number_of_lines = txt_lines.length;		
-	
-	//Initatilising arrays
-	//First column is field values
-	//Second column is absorption values
-	//Third colum is going to be the first derivative of col 2 wrt col 1
-
-	var eprData= [[0,0],[0,0],[0,0]];	//final data array containing col1 and col3
-	var firstCol = [],
-	 secondCol = [],
-	 thirdCol = [],
-	 tempData = [];	//temp array used to create 2d array
-
-	 
-	
-	
-	var str = "";
-	for (var i=0;i<number_of_lines-2;i++) {
-	//Lines are parsed into columns by specific formatting of my data
-	//Probably a more robust, more general way of doing this
-	firstCol[i] = parseFloat(txt_lines[i].substring(0,25));
-	secondCol[i] = parseFloat(txt_lines[i].substring(27,44));
-	
-	if (i >= 1) {
-
-	//Calcualtes derivative
-	thirdCol[i] = ((secondCol[i] - secondCol[i-1]) / (firstCol[i] - firstCol[i-1]));
-	
-	
-	tempData[0] = firstCol[i];
-	tempData[1] = thirdCol[i];
-	eprData[i] = tempData;	//push temp array into final data array
-
-	}
-	else {
-	
-	thirdCol[i] = 0;
-	tempData[0] = firstCol[i];
-	tempData[1] = thirdCol[i];
-	eprData[i] = tempData;	//push temp array into final data array
-	}
-	tempData = [];	//clear temp array
-	
-
-
-	}
-
-	d3Plot(eprData);	//send final 2d array to be plotted
-
-	}   
-
-d3Plot = function (eprData)	{  
-	var data = eprData;
-    if($('.chart')[0]){
-    d3.select("svg").remove();
-    }
-	//set margins for plot
-    var margin = {top:20, right: 15, bottom:60, left:60},
-    	width = 960 - margin.left- margin.right,
-    	height = 500 - margin.top - margin.bottom;
-
-    //set auto scale x based on data range
-    var x = d3.scale.linear()
-    			.domain([d3.min(data,function(d){return d[0];}),d3.max(data,function(d){return d[0];})])
-    			.range([0, width]);
-    //set auto scale y based on data range
-    var y = d3.scale.linear()
-    			.domain([d3.min(data,function(d){return d[1];}), d3.max(data,function(d){return d[1];})])
-    			.range([height, 0]);
-    //init chart svg
-    var chart = d3.select('#wrapper')
-    			.append('svg:svg')
-    			.attr('widht', width + margin.right + margin.left)
-    			.attr('height',height + margin.top + margin.bottom)
-    			.attr('class','chart span10');
-    //init main
-    var main = chart.append('g')
-    			.attr('transform','translate('+ margin.left + ',' + margin.top + ')')
-    			.attr('width',width)
-    			.attr('height',height)
-    			.attr('class','main');
-
-    //fn to draw line graph from data
-    var line = d3.svg.line()
-    			.x(function(d,i){
-    				return x(d[0]);
-    			})
-    			.y(function(d){
-    				return y(d[1]);
-    			})
-    //define x axis
-    var xAxis = d3.svg.axis()
-    			.scale(x)
-    			.orient('bottom');
-    //append x axis
-    	main.append('g')
-    		.attr('transform','translate(0,'+ height + ')')
-    		.attr('class','main axis date')
-    		.call(xAxis);
-   	//define y axis
-    var yAxis = d3.svg.axis()
-    				.scale(y)
-    				.orient('left');
-    	//append y axis			
-    	main.append('g')
-    		.attr('transform','translate(0,0)')
-    		.attr('class','main axis date')
-    		.attr(yAxis);
-    //add data to the graph
-    var g = main.append("svg:g");
-
-    	g.append("svg:path")
-    		.data(data)
-    		.attr("class","line")
-    		.attr("stroke","steelblue")
-    		.attr("fill","none")
-    		.attr("stroke-width",3)
-    		.attr("d",line(data));
-    		
-    	
-    //add dots on data points
-    	// g.selectAll("scatter-dots")
-    	// 	.data(data)
-    	// 	.enter().append("svg:circle")
-    	// 	.attr("cy",function(d){return y(d[1]);})
-    	// 	.attr("cx", function(d,i){return x(d[0]);})
-    	// 	.attr("r", 1)
-    	// 	.style("opacity", 1);
-
-  }
 function getOutput(fileName){
     $.get("server/get_output.php",{fileName:fileName},function(data){
                         
@@ -195,3 +62,218 @@ function getFileNames(){
     },"json")
 }
 
+parseSpectra = function (txt) {
+
+	var txt_lines = txt.split('\n');
+	var number_of_lines = txt_lines.length;		
+	
+	//Initatilising arrays
+	//First column is field values
+	//Second column is absorption values
+	//Third colum is going to be the first derivative of col 2 wrt col 1
+
+	var eprData= [[0,0],[0,0],[0,0]];	//final data array containing col1 and col3
+	var firstCol = [],
+	 secondCol = [],
+	 thirdCol = [],
+	 tempData = [];	//temp array used to create 2d array
+
+	 
+	
+	
+	var str = "";
+	for (var i=0;i<number_of_lines-2;i++) {
+	//Lines are parsed into columns by specific formatting of my data
+	//Probably a more robust, more general way of doing this
+	firstCol[i] = parseFloat(txt_lines[i].substring(0,25));
+	secondCol[i] = parseFloat(txt_lines[i].substring(27,44));
+	
+	if (i >= 1) {
+
+	//Calcualtes derivative
+	thirdCol[i] = ((secondCol[i] - secondCol[i-1]) / (firstCol[i] - firstCol[i-1]));
+	
+	
+	tempData[0] = firstCol[i];
+	tempData[1] = thirdCol[i];
+	eprData[i] = tempData;	//push temp array into final data array
+
+	}
+	else {
+	
+	thirdCol[i] = 0;
+	tempData[0] = firstCol[i];
+	tempData[1] = thirdCol[i];
+	eprData[i] = tempData;	//push temp array into final data array
+	}
+	tempData = [];	//clear temp array
+	
+
+
+	}
+	
+
+	var JSONstr = "[";
+	for (var i=0;i<number_of_lines-2;i++) {
+		if (i>0) {
+		JSONstr += ",";
+		}
+		JSONstr += '\n';
+		JSONstr += "{";
+		JSONstr += '"x":';
+		JSONstr += firstCol[i];
+		JSONstr += ',"y":';
+		JSONstr += thirdCol[i];
+		JSONstr += "}";
+	}
+	JSONstr += "]";
+	
+	//console.log(JSONstr);
+	//d3Plot(eprData);	//send final 2d array to be plotted
+	d3Plot_JSON(JSONstr);
+
+	}   
+
+
+d3Plot_JSON = function(JSONstr) { 
+
+margin = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 45
+};
+
+	data = JSON.parse(JSONstr);
+	
+	//Width and height
+	width = 480 - margin.left - margin.right;
+	height = 250 - margin.top - margin.bottom;
+
+	//Scale data to fit window
+	var x = d3.scale.linear()
+		.domain(d3.extent(data, function (d) {
+		return d.x;
+	}))
+		.range([0, width]);
+
+	var y = d3.scale.linear()
+		.domain(d3.extent(data, function (d) {
+		return d.y;
+	}))
+		.range([height, 0]);
+
+	//Describe line
+	var line = d3.svg.line()
+		.x(function (d) {
+		return x(d.x);
+	})
+		.y(function (d) {
+		return y(d.y);
+	});
+
+	//Zoom!
+	var zoom = d3.behavior.zoom()
+		.x(x)
+		.y(y)
+		.scaleExtent([0.9,10])
+		.on("zoom", zoomed);
+
+	d3.select("svg")
+		.remove();
+	//Init svg object
+	svg = d3.select('#chart')
+		.append("svg:svg")
+		.attr('width', width + margin.left + margin.right)
+		.attr('height', height + margin.top + margin.bottom)
+		.call(zoom)
+		.append("svg:g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	//Functions for scaling axes
+	var make_x_axis = function () {
+		return d3.svg.axis()
+			.scale(x)
+			.orient("bottom")
+			.ticks(16);
+	};
+
+	var make_y_axis = function () {
+		return d3.svg.axis()
+			.scale(y)
+			.orient("left")
+			.ticks(16);
+	};
+
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom")
+		.ticks(8);
+
+	svg.append("svg:g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0, " + height + ")")
+		.call(xAxis);
+
+	var yAxis = d3.svg.axis()
+		.scale(y)
+		.orient("left")
+		.ticks(5);
+		
+	//Put axes on chart
+
+	svg.append("g")
+		.attr("class", "y axis")
+		.call(yAxis);
+
+	svg.append("g")
+		.attr("class", "x grid")
+		.attr("transform", "translate(0," + height + ")")
+		.call(make_x_axis()
+		.tickSize(-height, 0, 0)
+		.tickFormat(""));
+
+	svg.append("g")
+		.attr("class", "y grid")
+		.call(make_y_axis()
+		.tickSize(-width, 0, 0)
+		.tickFormat(""));
+		
+	
+	//Render stuff when zoomed in
+
+	var clip = svg.append("svg:clipPath")
+		.attr("id", "clip")
+		.append("svg:rect")
+		.attr("x", 0)
+		.attr("y", 0)
+		.attr("width", width)
+		.attr("height", height);
+
+	var chartBody = svg.append("g")
+		.attr("clip-path", "url(#clip)");
+
+	chartBody.append("svg:path")
+		.datum(data)
+		.attr("class", "line")
+		.attr("d", line);
+
+	//Function for how to behave when zooming in
+	function zoomed() {
+		svg.select(".x.axis").call(xAxis);
+		svg.select(".y.axis").call(yAxis);
+		svg.select(".x.grid")
+			.call(make_x_axis()
+			.tickSize(-height, 0, 0)
+			.tickFormat(""));
+		svg.select(".y.grid")
+			.call(make_y_axis()
+			.tickSize(-width, 0, 0)
+			.tickFormat(""));
+		svg.select(".line")
+			.attr("class", "line")
+			.attr("d", line);
+	}
+
+
+}
